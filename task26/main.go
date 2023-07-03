a2c1ccd2de1403a95e694668b60eb4790997b198
lptnkv <lipatnikov.ks@gmail.com>
2023-07-03 14:43:28 +0300

    make case insensitive

--- task26/main.go
+++ task26/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode"
+)
 
 func main() {
 	s1 := "abcd"
@@ -8,17 +11,20 @@ func main() {
 	s3 := "aabcd"
 	s4 := "😀привет😀"
 	s5 := "привет😀"
+	s6 := "aAbB"
 	fmt.Println("s1: ", IsUnique(s1))
 	fmt.Println("s2: ", IsUnique(s2))
 	fmt.Println("s3: ", IsUnique(s3))
 	fmt.Println("s4: ", IsUnique(s4))
 	fmt.Println("s5: ", IsUnique(s5))
+	fmt.Println("s6: ", IsUnique(s6))
 }
 
 func IsUnique(s string) bool {
 	dict := make(map[rune]bool)
 	for _, val := range s {
-		if found := dict[val]; found {
+		lower := unicode.ToLower(val)
+		if found := dict[lower]; found {
 			return false
 		}
 		dict[val] = true
