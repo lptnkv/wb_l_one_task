67d2a75f90c15505598273a5050031be8c3a77fb
lptnkv <lipatnikov.ks@gmail.com>
2023-07-04 10:31:08 +0300

    add comments to task21

--- task21/main.go
+++ task21/main.go
@@ -7,24 +7,31 @@ func main() {
 	cardReader.connectWithUsbCable()
 }
 
+// Интерфейс для подключения через USB
 type USB interface {
 	connectWithUsbCable()
 }
 
+// Карта памяти
 type MemoryCard struct{}
 
+// Метод для вставки карты памяти
 func (card *MemoryCard) insert() {
 	fmt.Println("Карта памяти вставлена")
 }
 
+// Метод для копирования данных на карту
 func (card *MemoryCard) copyData() {
 	fmt.Println("Данные скопированы")
 }
 
+// Кардридер, который можно подключить через USB
+// Является адаптером для карты памяти
 type CardReader struct {
 	card MemoryCard
 }
 
+// Реализация метода интерфейса USB
 func (reader CardReader) connectWithUsbCable() {
 	reader.card.insert()
 	reader.card.copyData()
